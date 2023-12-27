925a73efb984047ca19bf3b8e0e703cb9ae89325
Timure <tima.goresnkiy@mail.ru>
2023-12-27 10:57:27 +0300

    ищем сумму чисел фибоначи наиболее приближенную к дано

--- consecutive_fib_numbers/cmd/main.go
+++ consecutive_fib_numbers/cmd/main.go
@@ -1,28 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	consecutive "timur/consecutive_fib_numbers"
+)
 
 func main() {
-	fmt.Println(ProductFib(74049690))
-}
-
-func ProductFib(prod uint64) (answer [3]uint64) {
-
-	prev := uint64(1)
-
-	for i := uint64(0); i*prev < prod; {
-
-		i = prev + i
-		prev = i - prev
-
-		if i*prev >= prod {
-			answer[0], answer[1] = prev, i
-
-			if i*prev == prod {
-				answer[2] = 1
-			}
-		}
-	}
-
-	return answer
+	fmt.Println(consecutive.ProductFib(74049690))
 }
